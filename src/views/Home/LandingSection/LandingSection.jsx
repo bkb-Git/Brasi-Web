f366c61acaa36ba9ce5cde95fb6c38f4aca82569
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2022-06-03 14:32:09 +0300

    added meta tags and image optimizations

--- src/views/Home/LandingSection/LandingSection.jsx
+++ src/views/Home/LandingSection/LandingSection.jsx
@@ -12,7 +12,7 @@ const LandingSection = () => {
   return (
     <Row className={style.landingPage}>
       <Col style={{ zIndex: 0 }} className={style.landingPage__imageContainer}>
-        <Image src={PortImage} alt="Port Image" style={{ zIndex: 0 }} />
+        <Image priority src={PortImage} alt="Port Image" style={{ zIndex: 0 }} />
       </Col>
       <Col className={style.landingPage__title}>
         <Title level={1}>Reliable Clearing & Forwarding Services</Title>
